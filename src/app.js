d3a39ba0f5d0dcbb8c087f0873f8a00f51c27240
nicol <nicolmadero12@gmaicom>
2021-02-24 11:01:13 -0500

    reverse

--- src/app.js
+++ src/app.js
@@ -1,20 +1,19 @@
 
-  var traer=function (numero) {
-  var numero=document.getElementById('numero').value;
+  var traer= function (numero) {
+  var numero =document.getElementById('numero').value;
   //var boton=document.getElementById('boton').nosetengo dudas y preguntas;
    var lista=Array.from(numero)
    var lista1=lista.length
    var listn=[]
+   
 
    if  (lista1 ===10){
 
      for (var i in lista){
        listn[i]=parseInt(lista[i])
      }
-     for (i in listn.length/2) {
-       listn[2*i]=2*listn[i*2]
-
-     }
+     
+     var listaalreves = listn.reverse();
      console.log(listn);
     listn[0,2,4,6,8]=(listn[0,2,4,6,8])*2
 
@@ -26,9 +25,10 @@
     alert("Debes introducir un numero");
   }
 
+
   console.log(numero+1);
   console.log(listn);
-  
+  console.log(listaalreves)
   //console.log(listan);
   //dentro de la pagina
   
