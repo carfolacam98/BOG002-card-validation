5cd5f4d9bd74cc4185a557f87ed94cf620ebca42
nicol <nicolmadero12@gmaicom>
2021-02-26 12:59:00 -0500

    validator

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
+const botonValidar = document.getElementById('boton');
+botonValidar.addEventListener('click',traernumero);
 
-console.log(validator);
+function traernumero(){
+ var numero = document.getElementById('numero').value;
+console.log(numero);   
+validator.isValid(numero)
+console.log(validator.isValid(numero))
+validator.maskify(numero)
+console.log(validator.maskify(numero))
+}
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,17 +1,75 @@
-const isValid= ()=> {
-  const n= "4083952015263";
-  if (n==="4083952015263") {
-    return true}
+const isValid= (numerodetarjeta)=> {
+  var lista=Array.from( numerodetarjeta)
+  var numero = numerodetarjeta
+  var listn=[]
+   var n1=0
+   var n3=0
+   var suma=0
+   var sumafor=0
+  
+   
+   if  (lista.length ===15 || lista.length === 16){
+     
+    
+     for (var i in lista){
+       listn[i]=parseInt(lista[i])
+       
+       
+     }
+     
+     var listaalreves = listn.reverse();
 
-};
+     
+     
+     
+     for (var i in listn){
+       if (i%2==0){
+         listn[i]=2*listn[i]}
+
+       if (listn[i]>9){
+          var n1=listn[i]%10
+          var n3= parseInt(listn[i]/10)
+          listn[i]= n3+ n1
+      }
+      sumafor=sumafor+listn[i]
+      }
+       if(sumafor%10==0){
+         alert("su tarjeta es valida")
+       }
+       else{alert("su tarjeta es invalida")}
+      }
+       
+    else{
+     alert("NOP");
+   }
+
+  if (numero== "") {
+    alert("Debes introducir un numero");
+  }
+  return sumafor
+  }
 
 
-const isinvalid= ()=>{
-  return false;
-};
+
+
+const maskify= (numerodetarjeta)=>{
+  var numero =numerodetarjeta
+  if (numero.length== 15 || numero.length== 16){
+    var ultimos4=numero.slice(-4);
+    var simbolo="#";
+    var tarjetaoculta=simbolo.repeat(numero.length-4)+ultimos4;
+
+  return tarjetaoculta;
+}
+  else{
+    var invalido ="ingrese mas numeros";
+    return invalido
+
+  }
+}
 const validator =  {
   isValid,
-  isinvalid
+  maskify
   
 };
 
