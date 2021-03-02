0b713f83c8ee08f03e9b3bfa2a53df36db562a0e
nicol <nicolmadero12@gmaicom>
2021-03-02 10:46:28 -0500

    boton

--- src/index.js
+++ src/index.js
@@ -16,10 +16,25 @@ console.log(validator.isValid(numero))
 validator.maskify(numero)
 console.log(validator.maskify(numero))
 }
-const botonpasar = document.getElementById('boton1');
-botonpasar.addEventListener('click',pasar1);
+//const botonpasar = document.getElementById('boton1');
+//botonpasar.addEventListener('click',pasar1);
+
 function pasar1(){
- var elem =document.getElementById("ultimoscreen");
- elem.style.display("block");
+ var elem=document.getElementById('ultimoscreen');
+ elem.style.display = "block";
+ var elem1 =document.getElementById('primerscreen');
+ elem1.style.display = "none";
 }
+document.getElementById('boton1').onclick = function (){
+    pasar1();
+    
+}
+ /*function pasar2(){
+     var elem3=document.getElementById('segundoscreen');
+     elem3.style.display = "block";
+ }
+ document.getElementById('boton1').onclick = function (){
+    pasar2();
+ }*/
+
 
